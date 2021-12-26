f9962bebafb445c58dab51351f7f28a254c4e897
Asad Khurshid <i211716@nu.edu.pk>
2021-12-26 22:49:28 +0500

    Added merging to dictionaries. They were overwriting before.

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import sys
 
 #Defining function for reeserving seats
-def reservation(number_of_tickets):
+def reservation(number_of_tickets, maxKey):
     dict = {}
-    for n in range(1, number_of_tickets+1):
+    for n in range(maxKey + 1, maxKey + number_of_tickets+1):
         name = input("Enter person's Name: ")
         age = input("Enter person's Age: ")
         gender = input("Enter person's Gender: ")
@@ -20,6 +20,7 @@ def generate_all_tickets(dict):
 #Defining main function
 def main():
     decision = 0
+    reserved_tickets = {}
     #initiating while loop to keep the porgram running
     while True:
         decision = input("If you want to reserver Tickets, Press 1.\nIf you want to generate all tickets, Press 2.\nIf you want to end the program, Enter end.\nType here: ")
@@ -27,7 +28,13 @@ def main():
             sys.exit()
         if int(decision) == 1:
             n_tickets = input("Enter the number of tickets you want to reserve: ")
-            reserved_tickets = reservation(int(n_tickets))
+            try:
+                max_key = max(reserved_tickets, key=reserved_tickets. get)
+                print (max_key)
+            except:
+                max_key = 0
+            tempDict = reservation(int(n_tickets), int(max_key))
+            reserved_tickets.update(tempDict)
         elif int(decision) == 2:
             print(generate_all_tickets(reserved_tickets))
 
