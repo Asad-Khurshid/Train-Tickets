81261e70f656a7ef3acd32a26e60029731959eb6
Asad Khurshid <i211716@nu.edu.pk>
2021-12-26 22:16:33 +0500

    Completed working state

---
+++ main.py
@@ -0,0 +1,36 @@
+#importing modules
+import sys
+
+#Defining function for reeserving seats
+def reservation(number_of_tickets):
+    dict = {}
+    for n in range(1, number_of_tickets+1):
+        name = input("Enter person's Name: ")
+        age = input("Enter person's Age: ")
+        gender = input("Enter person's Gender: ")
+        dict[n] = [name, age, gender]
+    return dict
+
+#Defining function for printing all reserved seats
+def generate_all_tickets(dict):
+    for seat in dict:
+        x = dict[seat]
+        print ("Ticket no:", seat, "is reserved for:", x[0], "Age:", x[1], "Gender:", x[2])
+
+#Defining main function
+def main():
+    decision = 0
+    #initiating while loop to keep the porgram running
+    while True:
+        decision = input("If you want to reserver Tickets, Press 1.\nIf you want to generate all tickets, Press 2.\nIf you want to end the program, Enter end.\nType here: ")
+        if decision == "end":
+            sys.exit()
+        if int(decision) == 1:
+            n_tickets = input("Enter the number of tickets you want to reserve: ")
+            reserved_tickets = reservation(int(n_tickets))
+        elif int(decision) == 2:
+            print(generate_all_tickets(reserved_tickets))
+
+#Executing program
+if __name__ == "__main__":
+    main()
